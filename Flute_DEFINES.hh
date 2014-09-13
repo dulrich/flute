b6d1a58e36985ddcb64178084e0caecaaa78ecb2
David Ulrich <dulrich@atomicinfotech.com>
2014-09-13 15:17:45 -0500

    remove unneeded enum

--- Flute_DEFINES.hh
+++ Flute_DEFINES.hh
@@ -4,11 +4,6 @@
 // the currently activated editor pane
 const int FLUTE_ACTIVE_PANE = -1;
 
-enum Flute_Buffer_State {	
-	FLUTE_BUFFER_ACTIVE = 1,
-	FLUTE_BUFFER_EDITED = 2
-};
-
 extern "C" {
 #include <lua5.2/lua.h>
 #include <lua5.2/lualib.h>
